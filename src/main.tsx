e362a2deb40b63705cdb0175f70988290264e166
Joel Liew <joelliewweiyi@gmail.com>
2026-03-19 17:43:21 +0100

    refresh

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Auto-reload when a new service worker activates (PWA update)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.location.reload();
+  });
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
